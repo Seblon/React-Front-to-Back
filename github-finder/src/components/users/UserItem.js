875ae397895b5385e343493c6df03833e8c2968e
Sebbe <jsbmedia@gmail.com>
2019-09-16 16:45:29 +0200

    Functional Components accomplished.

--- github-finder/src/components/users/UserItem.js
+++ github-finder/src/components/users/UserItem.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class UserItem extends Component {
-   render() {
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
 
-      const { login, avatar_url, html_url } = this.props.user;
-
-      return (
-         <div className='card text-center'>
-            <img src={avatar_url} alt='Profile' className="round-img" style={{ width: '60px'}} />
-            <h3>{login}</h3>
-            <div>
-               <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-            </div>
+   return (
+      <div className='card text-center'>
+         <img src={avatar_url} alt='Profile' className="round-img" style={{ width: '60px'}} />
+         <h3>{login}</h3>
+         <div>
+            <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
          </div>
-      );
-   }
+      </div>
+   );
+}
+
+UserItem.propTypes = {
+   user: PropTypes.object.isRequired,
 }
 
 export default UserItem
